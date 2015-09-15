68cf320dfbc67e6c69fb2415d52712926eff174e
Nayeema Lail <w0270771@nscc.ca>
2015-09-15 07:28:46 -0700

    Lab1PartA - added the files of guessing game to the folder without change

---
+++ Lab1PartA/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 9/15/15
+ * Time: 7:07 AM
+ */
+    $number1 = 5;
+    $number2 = 8;
+    $sum = $number1 + $number2;
+    echo "<h1>The sum is : ";
+    echo $sum;
+    echo "</h1>";
